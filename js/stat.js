99aee1905f9d65acf9b5074bf07e5ea4e6039f5e
kaiere <chupeykina.olga@gmail.com>
2020-02-11 16:24:56 +0900

    Определили функцию, вызываемую при прохождении уровня, внутри нее вызвали функции, отрисовывающие окно результатов

--- js/stat.js
+++ js/stat.js
@@ -10,3 +10,8 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, CLOUD_X + CLOUD_GAP, CLOUD_Y + CLOUD_GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+};
