9e71e6ee10ef142d79f6b9a859f8607d07fd8918
kaiere <chupeykina.olga@gmail.com>
2020-02-11 16:21:13 +0900

    Объявили функцию, отвечающую за отрисовку окна результатов

--- js/stat.js
+++ js/stat.js
@@ -5,3 +5,8 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var CLOUD_GAP = 10;
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
