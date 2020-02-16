71b9033e9421a9d99007073f34f7e672521376e7
kaiere <chupeykina.olga@gmail.com>
2020-02-16 18:30:26 +0900

    Нашли элемент, в который будут вставляться похожие маги

--- js/setup.js
+++ js/setup.js
@@ -8,3 +8,5 @@ var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', '�
 var COAT_COLORS = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARDS_AMOUNT = 4;
+
+var similarListElement = document.querySelector('.setup-similar-list');
