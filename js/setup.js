9c88f1013875efb9d586ba8e38ebfd75c6dded0b
kaiere <chupeykina.olga@gmail.com>
2020-02-16 18:39:13 +0900

    Вызвали функцию добавления магов на страницу, показали блок с магами, исправили ошибку с цветами

--- js/setup.js
+++ js/setup.js
@@ -5,7 +5,7 @@ userDialog.classList.remove('hidden');
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLORS = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARDS_AMOUNT = 4;
 
@@ -50,3 +50,5 @@ var addWizards = function () {
   similarListElement.appendChild(fragment);
 };
 
+addWizards();
+document.querySelector('.setup-similar').classList.remove('hidden');
