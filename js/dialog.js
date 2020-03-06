1f63993791ee5969c2ade97a6f8f4b42dcf5543f
kaiere <chupeykina.olga@gmail.com>
2020-03-06 22:38:00 +0900

    Поставили обработчик на иконку - нажатие мышки

--- js/dialog.js
+++ js/dialog.js
@@ -5,4 +5,14 @@
 (function () {
   var userDialog = document.querySelector('.setup');
   var upload = document.querySelector('.upload');
+
+  upload.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
 })();
