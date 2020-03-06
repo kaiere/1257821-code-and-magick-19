6ce7647efe574f6560a7820bd798cc91688f3b7c
kaiere <chupeykina.olga@gmail.com>
2020-03-06 22:49:18 +0900

    Исправили ошибки (Трэвис)

--- js/setup.js
+++ js/setup.js
@@ -2,43 +2,43 @@
 
 // Отрисовка братьев Пендальфа
 (function () {
-var similarListElement = document.querySelector('.setup-similar-list');
+  var similarListElement = document.querySelector('.setup-similar-list');
 
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-  .content
-  .querySelector('.setup-similar-item');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
 
-var getWizards = function (wizardsAmount) {
-  var wizards = [];
-  for (var i = 0; i < wizardsAmount; i++) {
-    wizards.push({
-      name: window.utils.getRandomArrayIndex(window.utils.WIZARD_NAMES) + ' ' + window.utils.getRandomArrayIndex(window.utils.WIZARD_SURNAMES),
-      coatColor: window.utils.getRandomArrayIndex(window.utils.COAT_COLORS),
-      eyesColor: window.utils.getRandomArrayIndex(window.utils.EYES_COLORS)
-    });
-  }
-  return wizards;
-};
+  var getWizards = function (wizardsAmount) {
+    var wizards = [];
+    for (var i = 0; i < wizardsAmount; i++) {
+      wizards.push({
+        name: window.utils.getRandomArrayIndex(window.utils.WIZARD_NAMES) + ' ' + window.utils.getRandomArrayIndex(window.utils.WIZARD_SURNAMES),
+        coatColor: window.utils.getRandomArrayIndex(window.utils.COAT_COLORS),
+        eyesColor: window.utils.getRandomArrayIndex(window.utils.EYES_COLORS)
+      });
+    }
+    return wizards;
+  };
 
-var renderWizard = function (wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
-  return wizardElement;
-};
+    return wizardElement;
+  };
 
-var addWizards = function () {
-  var wizards = getWizards(window.utils.WIZARDS_AMOUNT);
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < wizards.length; i++) {
-    fragment.appendChild(renderWizard(wizards[i]));
-  }
-  similarListElement.appendChild(fragment);
-};
+  var addWizards = function () {
+    var wizards = getWizards(window.utils.WIZARDS_AMOUNT);
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < wizards.length; i++) {
+      fragment.appendChild(renderWizard(wizards[i]));
+    }
+    similarListElement.appendChild(fragment);
+  };
 
-addWizards();
-document.querySelector('.setup-similar').classList.remove('hidden');
+  addWizards();
+  document.querySelector('.setup-similar').classList.remove('hidden');
 })();
