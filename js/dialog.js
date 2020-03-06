946084f73baadcbf42dc50dbd239c44680cbf827
kaiere <chupeykina.olga@gmail.com>
2020-03-06 22:39:17 +0900

    Написали функцию для перемещения окна при движении мышки

--- js/dialog.js
+++ js/dialog.js
@@ -15,4 +15,25 @@
     };
 
     var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      if (shift.x !== 0 && shift.y) {
+        dragged = true;
+      }
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      userDialog.style.top = (userDialog.offsetTop - shift.y) + 'px';
+      userDialog.style.left = (userDialog.offsetLeft - shift.x) + 'px';
+    };
 })();
