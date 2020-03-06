5e78a759d87e0a54e827f6e6f827fb21ba088fc7
kaiere <chupeykina.olga@gmail.com>
2020-03-06 22:21:34 +0900

    Убрали из setup.js элементы, перенесенные в новый файл

--- js/setup.js
+++ js/setup.js
@@ -1,21 +1,5 @@
 'use strict';
 
-var userDialog = document.querySelector('.setup');
-// userDialog.classList.remove('hidden');
-
-var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
-var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-var WIZARDS_AMOUNT = 4;
-var ENTER_KEY = 'Enter';
-var ESC_KEY = 'Escape';
-
-var getRandomArrayIndex = function (arrayName) {
-  return arrayName[Math.floor(Math.random() * arrayName.length)];
-};
-
 var similarListElement = document.querySelector('.setup-similar-list');
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
