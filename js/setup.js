276c3ee03d03ba4ed769ab7c14fc4a02d11fecaf
kaiere <chupeykina.olga@gmail.com>
2020-03-11 18:35:44 +0900

    Добавили код отрисовки подгруженных с сервера магов

--- js/setup.js
+++ js/setup.js
@@ -2,31 +2,51 @@
 
 // Отрисовка братьев Пендальфа
 (function () {
+  var userDialog = document.querySelector('.setup');
   var similarListElement = document.querySelector('.setup-similar-list');
-
-  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
   var renderWizard = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
 
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
 
     return wizardElement;
   };
 
-  var addWizards = function () {
-    var wizards = getWizards(window.utils.WIZARDS_AMOUNT);
+  var successHandler = function (wizards) {
     var fragment = document.createDocumentFragment();
-    for (var i = 0; i < wizards.length; i++) {
-      fragment.appendChild(renderWizard(wizards[i]));
+
+    for (var i = 0; i < window.utils.WIZARDS_AMOUNT; i++) {
+      fragment.appendChild(renderWizard(window.utils.getRandomArrayIndex(wizards)));
     }
     similarListElement.appendChild(fragment);
+
+    userDialog.querySelector('.setup-similar').classList.remove('hidden');
   };
 
-  addWizards();
-  document.querySelector('.setup-similar').classList.remove('hidden');
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  var form = userDialog.querySelector('.setup-wizard-form');
+  form.addEventListener('submit', function (evt) {
+    window.backend.save(new FormData(form), function () {
+      window.dialog.closeWindow();
+    });
+
+    evt.preventDefault();
+  });
+
+  window.backend.load(successHandler, errorHandler);
 })();
