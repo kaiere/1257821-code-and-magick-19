1f7c2e9b134654e98c5d3722a72725dc70cd7a28
kaiere <chupeykina.olga@gmail.com>
2020-03-14 23:22:10 +0900

    Вынесли в модуль функции для фильтрации волшебников

---
+++ js/similar.js
@@ -0,0 +1,70 @@
+'use strict';
+
+(function () {
+
+  var wizards = [];
+  var coatColor;
+  var eyesColor;
+
+  var getRank = function (wizard) {
+    var rank = 0;
+    if (wizard.colorCoat === coatColor) {
+      rank += 2;
+    }
+    if (wizard.colorEyes === eyesColor) {
+      rank += 1;
+    }
+    return rank;
+  };
+
+  var compareNames = function (left, right) {
+    if (left > right) {
+      return 1;
+    } else if (left < right) {
+      return -1;
+    } else {
+      return 0;
+    }
+  };
+
+  var updateWizards = function () {
+    var uniqueWizards = wizards.sort(function (left, right) {
+      var rankDiff = getRank(right) - getRank(left);
+      if (rankDiff === 0) {
+        rankDiff = compareNames(left.name, right.name);
+      }
+      return rankDiff;
+    });
+
+    window.render.renderWizard(uniqueWizards);
+  };
+
+  window.wizard.onEyesChange = window.debounce(function (color) {
+    eyesColor = color;
+    updateWizards();
+  });
+
+  window.wizard.onCoatChange = window.debounce(function (color) {
+    coatColor = color;
+    updateWizards();
+  });
+
+  var onLoad = function (data) {
+    wizards = data;
+    updateWizards();
+  };
+
+  var onError = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.backend.load(onLoad, onError);
+})();
